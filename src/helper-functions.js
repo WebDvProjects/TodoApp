fa59517e786985ae812e924dbf81410cb2d238ef
Cassian <kahemacassian@gmail.com>
2023-01-06 09:09:42 +0000

    fix media queries

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -5,8 +5,19 @@ const veil = document.createElement("div");
 veil.classList.add("veil");
 veil.addEventListener("click", () => {
   // remove pop up if veil is clicked
-  document.querySelector(".popup").remove();
+  const popup = document.querySelector(".popup");
+  // if (popup.classList.contains('side-bar')) return;
+  popup?.remove();
   // todo if under 560px and sidemenu is open then close it (set the hamburger icon to inactive)
+  let mql = window.matchMedia("(max-width: 560px)");
+  if (mql.matches) {
+    const hamburger = document.querySelector(".menu-icon");
+    const sideMenu = document.querySelector(".side-bar");
+    if (hamburger.classList.contains("active")) {
+      hamburger.classList.remove("active");
+      sideMenu.classList.remove("active");
+    }
+  }
 });
 document.querySelector(".content").append(veil);
 
